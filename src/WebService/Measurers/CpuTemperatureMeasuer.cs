a4597fbb58931f860f367d7569f6e4a1fca74f92
ktomkow <krzysztof.tomkow@gmail.com>
2021-09-10 10:59:27 +0200

    Added logging

--- src/WebService/Measurers/CpuTemperatureMeasuer.cs
+++ src/WebService/Measurers/CpuTemperatureMeasuer.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
 using WebService.Metrics;
 
 namespace WebService.Measurers
@@ -6,15 +7,19 @@ namespace WebService.Measurers
     public class CpuTemperatureMeasuer : Measurer<double>
     {
         private Iot.Device.CpuTemperature.CpuTemperature measurer;
+        private readonly ILogger<CpuTemperatureMeasuer> logger;
 
-        public CpuTemperatureMeasuer(Iot.Device.CpuTemperature.CpuTemperature measurer, Summaries summaries) : base(summaries)
+        public CpuTemperatureMeasuer(Iot.Device.CpuTemperature.CpuTemperature measurer, Summaries summaries, ILogger<CpuTemperatureMeasuer> logger) : base(summaries)
         {
             this.measurer = measurer;
+            this.logger = logger;
         }
 
         protected override async Task<double> ReadValue()
         {
-            return await Task.FromResult(measurer.Temperature.DegreesCelsius);
+            double temperature = measurer.Temperature.DegreesCelsius;
+            this.logger.LogInformation($"Temperature: {temperature}∘C");
+            return await Task.FromResult(temperature);
         }
     }
 }
